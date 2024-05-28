47e8c08a904d8c4851e3d51f07d316eae05bd972
Jothish-nat <133888013+Jothish-nat@users.noreply.github.com>
2024-05-28 11:51:15 +0530

    Add files via upload
    Day-03 Home Task

---
+++ Day-01/01-SplitString.js
@@ -0,0 +1,7 @@
+const myString = "Hello World";
+const str = myString.split(" ");
+let len = str.length;
+let lastWord = str.length;
+console.log(str);
+console.log(lastWord);
+console.log(len);
---
+++ Day-01/02-TrimString.js
@@ -0,0 +1,22 @@
+var str = " fly me to the moon ";
+
+//Trim the String
+var trimmedstr = str.trim();
+console.log(trimmedstr);
+
+//Split the String
+var myString = str.split('');
+console.log(myString);
+
+//Identify the last word
+const lastword = myString.pop();
+console.log(lastword);
+
+//calculate the Length of the last word
+const lengthoflastword = str.length-1;
+console.log(lengthoflastword);
+
+//Return the length
+return lengthoflastword;
+
+
